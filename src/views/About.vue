423a069974e2a84b8df6ecef8d350120744c12a1
Jimison-TW <ex3067878@gmail.com>
2019-07-30 09:05:12 +0800

    暫存變更

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <NextIcon />
   </div>
 </template>
+
+<script>
+import NextIcon from '@/assets/icon/functions-next.svg'
+
+export default {
+  components: {
+    NextIcon
+  }
+}
+</script>
