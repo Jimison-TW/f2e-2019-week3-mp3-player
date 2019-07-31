ea89ffdedaabb882f6188d0980b262fada72cc7f
Jimison-TW <ex3067878@gmail.com>
2019-07-31 09:21:29 +0800

    暫存變更

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="outer-container">
+      <div id="sidebar">
+        <div v-for="(item, index) in btn_list" :key="index">
+          <sidebar-button :iconName="item.name" :buttonText="item.text" />
+        </div>
+      </div>
+      <div id="content"></div>
+    </div>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
+<script>
+import SidebarButton from '@/components/SidebarButton'
+
+export default {
+  name: 'app',
+  components: {
+    SidebarButton
+  },
+  data: () => ({
+    btn_list: [
+      { name: 'icon-home', text: '首頁' },
+      { name: 'icon-home', text: '首頁' },
+      { name: 'icon-home', text: '首頁' }
+    ]
+  })
+}
+</script>
+
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  background-color: black;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  height: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#outer-container {
+  display: table;
+  width: 100%;
+  height: 100%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#sidebar {
+  display: table-cell;
+  width: 15%;
+  height: 100%;
+  vertical-align: top;
+  background-color: brown;
+}
+
+#content {
+  display: table-cell;
+  width: 85%;
+  vertical-align: top;
 }
 </style>
---
+++ src/components/SidebarButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="button-container">
+    <icon :name="iconName" scale="100" style="color:#010101"></icon>
+    <p>{{ buttonText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar-button',
+  props: {
+    iconName: String,
+    buttonText: String
+  }
+}
+</script>
+
+<style scoped>
+.button-container {
+  width: 100px;
+  height: 100px;
+}
+</style>
